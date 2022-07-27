f23666d60df243de74485260f921e391ee43200d
Amir Hosein Nazari <nazari.amirhosein79@gmail.com>
2022-07-27 18:40:17 +0400

    first commit

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const path = require('path');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views/pug'));
+app.set('view engine', 'pug');
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
