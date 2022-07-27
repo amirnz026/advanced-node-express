9ce196e5ea122b1bc636f3a8179d2bf7b298114c
Amir Hosein Nazari <nazari.amirhosein79@gmail.com>
2022-07-27 19:16:48 +0400

    added passport and express-session

--- server.js
+++ server.js
@@ -2,15 +2,17 @@
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const path = require('path');
 
 const app = express();
-
-app.set('views', path.join(__dirname, 'views/pug'));
 app.set('view engine', 'pug');
+
+fccTesting(app); // For fCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
@@ -20,18 +22,18 @@ app.use(
     cookie: { secure: false },
   })
 );
-app.use(passport.initialize());
 
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please login' });
+  // Change the response to render the Pug template
+  res.render(process.cwd() + '/views/pug/index', {
+    title: 'Hello',
+    message: 'Please login',
+  });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port ' + process.env.PORT);
 });
