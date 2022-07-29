dd455039d02db86271b748b6dd889f2d2a01b48f
Amir Hosein Nazari <nazari.amirhosein79@gmail.com>
2022-07-30 01:12:30 +0400

    serialized the user login session

--- server.js
+++ server.js
@@ -26,24 +26,35 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
 
-passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-  //   done(null, null);
-  // });
-  done(null, null);
-});
+myDB(async (client) => {
+  const myDataBase = await client.db('database').collection('users');
 
-app.route('/').get((req, res) => {
-  // Change the response to render the Pug template
-  res.render(process.cwd() + '/views/pug/index', {
-    title: 'Hello',
-    message: 'Please login',
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('pug', {
+      title: 'Connected to Database',
+      message: 'Please login',
+    });
+  });
+
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+  // Be sure to add this...
+}).catch((e) => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
   });
 });
+// app.listen out here...
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port ' + process.env.PORT);
