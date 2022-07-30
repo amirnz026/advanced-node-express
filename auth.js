f708f685435e34d5b956f7db901520c53a4bf76c
Amir Hosein Nazari <nazari.amirhosein79@gmail.com>
2022-07-30 13:15:49 +0400

    added github registration strategy

--- auth.js
+++ auth.js
@@ -36,10 +36,36 @@ module.exports = function (app, myDataBase) {
       {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: '/auth/github/callback',
+        callbackURL:
+          'https://advanced-node-express-nz.herokuapp.com/auth/github/callback',
       },
       function (accessToken, refreshToken, profile, cb) {
         console.log(profile);
+        myDataBase.findOneAndUpdate(
+          { id: profile.id },
+          {
+            $setOnInsert: {
+              id: profile.id,
+              name: profile.displayName || 'John Doe',
+              photo: profile.photos[0].value || '',
+              email: Array.isArray(profile.emails)
+                ? profile.emails[0].value
+                : 'No public email',
+              created_on: new Date(),
+              provider: profile.provider || '',
+            },
+            $set: {
+              last_login: new Date(),
+            },
+            $inc: {
+              login_count: 1,
+            },
+          },
+          { upsert: true, new: true },
+          (err, doc) => {
+            return cb(null, doc.value);
+          }
+        );
       }
     )
   );
